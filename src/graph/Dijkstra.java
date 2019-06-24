b2fd8f07df67c374bd2664697df8454971917b0c
Terunori-Hashizume <hashizume.terunori@gmail.com>
2019-06-24 22:34:46 +0900

    fix dijkstra

--- src/graph/Dijkstra.java
+++ src/graph/Dijkstra.java
@@ -7,54 +7,59 @@ import java.util.PriorityQueue;
 
 public class Dijkstra {
     private static int INF = Integer.MAX_VALUE;
-    
+
     private int[] dst;
     private boolean[] done;
     private List<List<int[]>> nodes;
     private PriorityQueue<int[]> vertexQ;
-    
+
     public Dijkstra(int n) {
         this.dst = new int[n];
         this.done = new boolean[n];
         this.nodes = new ArrayList<>();
-        this.vertexQ = new PriorityQueue<>((u, v) -> u[1] - v[1]);
-        
-        Arrays.fill(dst, INF);
-        Arrays.fill(done, false);
-        
         for (int i = 0; i < n; ++i) {
             nodes.add(new ArrayList<>());
         }
+        this.vertexQ = new PriorityQueue<>((u, v) -> u[1] - v[1]);
     }
-    
+
+    private void init() {
+        Arrays.fill(dst, INF);
+        Arrays.fill(done, false);
+        vertexQ.clear();
+    }
+
     public int getDistance(int node) {
         return dst[node] < INF ? dst[node] : -1;
     }
-    
+
     public void setDistances(int sp) {
+        init();
+
         dst[sp] = 0;
-        vertexQ.add(new int[] {sp, dst[sp]});
-        
+        vertexQ.add(new int[] { sp, dst[sp] });
+
         while (!vertexQ.isEmpty()) {
             int[] cur = vertexQ.poll();
             int u = cur[0];
             done[u] = true;
-            
-            if (dst[u] < cur[1]) continue;
-            
+
+            if (dst[u] < cur[1])
+                continue;
+
             for (int[] node : nodes.get(u)) {
                 int v = node[0];
-                if (done[v]) continue;
+                if (done[v])
+                    continue;
                 if (dst[v] > dst[u] + node[1]) {
                     dst[v] = dst[u] + node[1];
-                    vertexQ.add(new int[] {v, dst[v]});
+                    vertexQ.add(new int[] { v, dst[v] });
                 }
             }
         }
     }
-    
+
     public void addEdge(int u, int v, int l) {
-        nodes.get(u).add(new int[] {v, l});
-        nodes.get(v).add(new int[] {u, l});
+        nodes.get(u).add(new int[] { v, l });
     }
 }
